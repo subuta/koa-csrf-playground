2e1120ac765226fa8f5584351ec2ab250103a7c7
subuta <shunta.3110@gmail.com>
2019-11-18 22:21:37 +0900

    Add malicious site example.

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ open http://localhost:3000
 # Open example page (SPA Version)
 open http://localhost:3000
 ```
+
+### How to test CSRF
+
+```bash
+# Start server with CSRF attack content.
+npm run startm
+
+# Add these records to `/etc/hosts` (for simulate attack from other domain)
+sudo echo "127.0.0.1 example.local" >> /etc/hosts
+
+# Toggle CSRF protection of root/index.js, and see what happens with(or without) CSRF protection.
+# Open malicious-site
+open http://example.local:4000/spa
+```
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import logger from 'koa-logger'
 import Router from 'koa-router'
 import serve from 'koa-static'
 
+import cors from '@koa/cors'
+
 import { source } from 'common-tags'
 
 import path from 'path'
@@ -20,6 +22,8 @@ const app = new Koa()
 app.keys = ['secret']
 
 app.use(serve(path.resolve(__dirname, './public')))
+
+app.use(cors())
 
 app.use(logger())
 app.use(koaBody())
@@ -50,12 +54,12 @@ router.get('/', (ctx) => {
               <a href='/spa.html'>SPA version</a>
             </header>
         
-            <form action="/register" method="POST">
-                <input type="hidden" name="_csrf" value="${ctx.csrf}" />
+            <form action='/register' method='POST'>
+                <input type='hidden' name='_csrf' value='${ctx.csrf}' />
                 <!-- We won't use these fields, but for realistic example :) -->
-                <input type="email" name="email" placeholder="Email" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Register</button>
+                <input type='email' name='email' placeholder='Email' />
+                <input type='password' name='password' placeholder='Password' />
+                <button type='submit'>Register</button>
             </form>
         </body>
     </html>
@@ -64,6 +68,14 @@ router.get('/', (ctx) => {
 
 // API to ensure CSRF token set.
 router.get('/api/ensure_csrf_token', (ctx) => {
+  ctx.cookies.set('some-cookie-value', 'hoge', {
+    // Treat as session cookie.
+    // Must force HTTPS at production
+    secure: !dev,
+    // Set httpOnly=false for enable reading cookie at client-side as deliberately.
+    httpOnly: false
+  })
+
   // SEE: https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
   ctx.cookies.set('XSRF-TOKEN', ctx.csrf, {
     // Treat as session cookie.
@@ -81,6 +93,7 @@ router.get('/register', (ctx) => {
 })
 
 router.post('/register', (ctx) => {
+  console.log('some-cookie-value', JSON.stringify(ctx.cookies.get('some-cookie-value'), null, 2))
   console.log('body = ', JSON.stringify(ctx.request.body, null, 2))
   // Your browser will show this message if CSRF check passed.
   ctx.body = 'success!'
